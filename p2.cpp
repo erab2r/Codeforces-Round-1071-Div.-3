015f65cdfd5b735b5977a153a92ebdfa8719cb59
erab2r <shkera213@gmail.com>
2026-01-20 22:28:38 +0600

    1

--- p2.cpp
+++ p2.cpp
@@ -1,22 +1,42 @@
 #include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+#define sz(yo) (int)yo.size()
+ 
+void solve() {
+   ll n;
+   cin >> n;
+   if (n == 2) {
+      cout << 2 << '\n';
+   }
+   else if (n == 3) {
+      cout << 3 << '\n';
+   }
+   else if ((n % 2 == 0) && (n / 2) % 2 == 0) {
+      cout << 0 << '\n';
+   }
+   else if ((n % 3 == 0) && (n / 3) % 2 == 0) {
+      cout << 0 << '\n';
+   }
+   else {
+      ll x = n / 2, y = n - x;
+      cout << abs(x - y) << '\n';
+   }
+}
+ 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n;
-        cin >> n;
-        if (n % 4 == 0) {
-            cout << 0 << '\n';
-        } else if (n % 4 == 2) {
-            cout << 2 << '\n';
-        } else {
-            cout << 1 << '\n';
-        }
-    }
-    return 0;
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+ 
+   int t;
+   t = 1;
+   cin >> t;
+   for (int cs = 1; cs <= t; cs++) {
+      solve();
+   }
+   return 0;
 }
